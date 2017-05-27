f1117f0648d232c0cb01ea9032657594907f374e
Gabor Szabo <gabor@szabgab.com>
2017-05-27 23:24:29 +0300

    post

--- bin/app.pl
+++ bin/app.pl
@@ -1,14 +1,28 @@
 use v6;
 use Bailador;
+use JSON::Fast;
 
 get '/' => sub {
+    my $root = $*PROGRAM-NAME.IO.absolute.IO.dirname.IO.dirname;
     template("index.tmpl", {
+        title      => 'Perl 6 Bailador based TODO',
+    });
+}
+
+post '/add' => sub {
+    my $text = request.params<text>;
+    #store($text);
+    template("added.tmpl", {
         title      => 'Perl 6 Bailador based TODO',
     });
 }
 
 get '/about' => sub {
     template("about.tmpl");
+}
+
+sub store(Str $text) {
+    
 }
 
 
