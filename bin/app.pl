aa660b3fbd21a5b362e95943a439f91ba59834ed
Gabor Szabo <gabor@szabgab.com>
2017-05-31 10:01:42 +0300

    fix todo saving

--- bin/app.pl
+++ bin/app.pl
@@ -2,16 +2,20 @@ use v6;
 use Bailador;
 use JSON::Fast;
 
+my $root = $*PROGRAM-NAME.IO.absolute.IO.dirname.IO.dirname;
+my $db   = $root.IO.child("todo.json");
+
 get '/' => sub {
-    my $root = $*PROGRAM-NAME.IO.absolute.IO.dirname.IO.dirname;
+    my @todo = read_list();
     template("index.tmpl", {
         title      => 'Perl 6 Bailador based TODO',
+        list       => @todo,
     });
 }
 
 post '/add' => sub {
     my $text = request.params<text>;
-    #store($text);
+    store($text);
     template("added.tmpl", {
         title      => 'Perl 6 Bailador based TODO',
     });
@@ -22,7 +26,26 @@ get '/about' => sub {
 }
 
 sub store(Str $text) {
-    
+    my @todo;
+    if $db.e {
+        @todo = read_list();
+    }
+
+    @todo.push: {
+        text => $text,
+        added => time,
+    };
+    my $json_str = to-json @todo;
+
+    $db.spurt($json_str);
+
+    return;
+}
+
+sub read_list() {
+    my $json_str = $db.slurp: :close;
+    my @todo = | from-json $json_str;
+    return @todo;
 }
 
 
